d49f8d97ba6b239588e8ecae0bdb10456a1c7bef
ghi <dokuwiki@imz.re>
2015-02-11 22:16:48 +0100

    fix

--- admin.php
+++ admin.php
@@ -27,7 +27,6 @@ class admin_plugin_bez extends DokuWiki_Admin_Plugin {
 	  $sqlite = new Connect();
 
 		$db_name = preg_replace('/[^a-zA-Z0-9]/', '', $_SERVER['SERVER_NAME']);
-	  $db_name = 'simisowikieu';
 
 		$m = new MongoClient();
 		$mongo = $m->selectDB($db_name);
