6c0e96600859dd839fd0e442a2eba451cfd0bd5b
ghi <dokuwiki@imz.re>
2016-03-30 17:01:36 +0200

    fix

--- tpl/issue_causes.php
+++ tpl/issue_causes.php
@@ -7,4 +7,10 @@
 			<?php include "cause.php" ?>
 		<?php endforeach ?>
 	</div>
+	<?php if ($template['issue']['raw_state'] == 0 && ($helper->user_coordinator($template['issue']['id']) || isset($nparams['tid']))): ?>
+		<a href="?id=<?php echo $this->id('cause_form', 'id', $template['issue']['id']) ?>">
+			<?php echo $bezlang['add_cause'] ?>
+		</a>
+	<? endif ?>
+</div>
 
