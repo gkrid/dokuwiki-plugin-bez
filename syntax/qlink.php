e9b4a37022002b20430d4804371662eb9a2d33ad
Szymon Olewniczak <solewniczak@imz.re>
2017-05-24 23:31:42 +0200

    clean

--- syntax/qlink.php
+++ syntax/qlink.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Plugin Now: Inserts a timestamp.
- * 
- * @license    GPL 3 (http://www.gnu.org/licenses/gpl.html)
- */
 // must be run within DokuWiki
 if(!defined('DOKU_INC')) die();
 
