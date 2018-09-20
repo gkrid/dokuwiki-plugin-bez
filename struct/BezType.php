9b93b298b733352a4f0b80f5605eb1f78a853271
Szymon Olewniczak <solewniczak@rid.pl>
2018-09-20 15:12:31 +0200

    qlikns for comments

--- struct/BezType.php
+++ struct/BezType.php
@@ -7,6 +7,13 @@ use dokuwiki\plugin\struct\types\AbstractMultiBaseType;
 
 class BezType extends AbstractMultiBaseType {
 
+    const BEZ_TABLE_CODES = array(
+        '' => 'thread',
+        'k' => 'thread_comment',
+        'z' => 'task',
+        'zk' => 'task_comment'
+    );
+
     protected $config = array(
         'autocomplete' => array(
             'maxresult' => 10
@@ -22,15 +29,42 @@ class BezType extends AbstractMultiBaseType {
      * @return bool true if $mode could be satisfied
      */
     public function renderValue($value, \Doku_Renderer $R, $mode) {
+        preg_match('/#([a-z]*)([0-9]+)/', $value, $matches);
+        list(,$code, $id) = $matches;
         $title = $value;
-        $id = substr($value, 1);//remove #
-        if ($id[0] == 'z') {
-            $id = substr($id, 1);
-            $wl = wl("bez:task:tid:$id");
-        } else {
-            $wl = wl("bez:thread:id:$id");
+
+        $anchor = '';
+        $id_key = 'id';
+        switch ($code) {
+            case '':
+                $table = 'thread';
+                break;
+            case 'k':
+                $table = 'thread';
+                $anchor = '#k' . $id;
+
+                /** @var helper_plugin_sqlite $sqlite */
+                $sqlite = plugin_load('helper', 'bez_db')->getDB();
+                $res = $sqlite->query("SELECT thread_id FROM thread_comment WHERE id=?", $id);
+                $id = $res->fetchColumn();
+                break;
+            case 'z':
+                $table = 'task';
+                $id_key = 'tid';
+                break;
+            case 'zk':
+                $table = 'task';
+                $id_key = 'tid';
+                $anchor = '#zk' . $id;
+
+                /** @var helper_plugin_sqlite $sqlite */
+                $sqlite = plugin_load('helper', 'bez_db')->getDB();
+                $res = $sqlite->query("SELECT task_id FROM task_comment WHERE id=?", $id);
+                $id = $res->fetchColumn();
+                break;
         }
 
+        $wl = wl("bez:$table:$id_key:$id") . $anchor;
         $R->doc .= '<a href="'.$wl.'">'.$title.'</a>';
 
 
@@ -44,20 +78,18 @@ class BezType extends AbstractMultiBaseType {
      * @return string
      */
     public function validate($rawvalue) {
-        $value = trim($rawvalue);
-
-        $id = substr($value, 1);//remove #
-        if ($id[0] == 'z') {
-            $id = substr($id, 1);
-            $table = 'task';
-        } else {
-            $table = 'thread';
-        }
+        preg_match('/#([a-z]*)([0-9]+)/', $rawvalue, $matches);
+        list(,$code, $id) = $matches;
 
         if (!is_numeric($id)) {
             throw new ValidationException('Invalid BEZ reference');
         }
 
+        if (!in_array($code, array_keys(self::BEZ_TABLE_CODES))) {
+                throw new ValidationException('Invalid BEZ reference');
+        }
+
+        $table = self::BEZ_TABLE_CODES[$code];
 
         /** @var helper_plugin_sqlite $sqlite */
         $sqlite = plugin_load('helper', 'bez_db')->getDB();
@@ -83,16 +115,13 @@ class BezType extends AbstractMultiBaseType {
         if(utf8_strlen($lookup) < 1) return array();
         if ($lookup[0] != '#') return array();
 
-        $id = substr($lookup, 1);
-        if ($id[0] == 'z') {
-            $id = substr($id, 1);
-            $table = 'task';
-        } else {
-            $table = 'thread';
-        }
+        preg_match('/#([a-z]*)([0-9]+)/', $lookup, $matches);
+        list(,$code, $id) = $matches;
 
         if (!is_numeric($id)) return array();
+        if (!in_array($code, array_keys(self::BEZ_TABLE_CODES))) return array();
 
+        $table = self::BEZ_TABLE_CODES[$code];
 
         // results wanted?
         $max = (int)$this->config['autocomplete']['maxresult'];
@@ -102,20 +131,22 @@ class BezType extends AbstractMultiBaseType {
 
         /** @var helper_plugin_sqlite $sqlite */
         $sqlite = $bez_db_helper->getDB();
-        $sql = "SELECT id, state FROM $table WHERE id LIKE ? LIMIT $max";
+
+        $fields = array('id');
+        if ($table == 'thread' || $table == 'task') {
+            $fields[] = 'state';
+        }
+
+        $sql = "SELECT " . implode(',', $fields) . " FROM $table WHERE id LIKE ? LIMIT $max";
         $res = $sqlite->query($sql, $id . '%');
 
         $result = array();
         while ($row = $res->fetch(\PDO::FETCH_ASSOC)) {
-            $state = $bez_db_helper->getLang('state_' . $row['state']);
 
-            $name = $row['id'] . ' (' . $state . ')';
-            if ($table == 'task') {
-                $name = '#z' . $name;
-                $value = '#z' . $row['id'];
-            } else {
-                $name = '#' . $name;
-                $value = '#' . $row['id'];
+            $name = $value = '#' . $code . $row['id'];
+            if ($table == 'thread' || $table == 'task') {
+                $state = $bez_db_helper->getLang('state_' . $row['state']);
+                $name .= ' (' . $state . ')';
             }
 
             $result[] = array(
--- syntax/qlink.php
+++ syntax/qlink.php
@@ -13,22 +13,49 @@ class syntax_plugin_bez_qlink extends DokuWiki_Syntax_Plugin {
     function getType() { return 'substition'; }
     function getSort() { return 34; }
 	function connectTo($mode) {
-		$this->Lexer->addSpecialPattern('#[z]?[0-9]+',$mode,'plugin_bez_qlink');
+		$this->Lexer->addSpecialPattern('#(?:z|zk|k)?[0-9]+',$mode,'plugin_bez_qlink');
 	}
 
 
     function handle($match, $state, $pos, Doku_Handler $handler) {
-		$code = substr($match, 1, 1);
-		if ($code === 'z') {
-			$nr = substr($match, 2, strlen($match));
-			return array('z', $nr);
-		} else {
-			$nr = substr($match, 1, strlen($match));
-			return array('', $nr);
-		}
+        preg_match('/#([a-z]*)([0-9]+)/', $match, $matches);
+        list(,$code, $id) = $matches;
+
+        $anchor = '';
+        $id_key = 'id';
+        switch ($code) {
+            case '':
+                $table = 'thread';
+                break;
+            case 'k':
+                $table = 'thread';
+                $anchor = '#k' . $id;
+
+                /** @var helper_plugin_sqlite $sqlite */
+                $sqlite = plugin_load('helper', 'bez_db')->getDB();
+                $res = $sqlite->query("SELECT thread_id FROM thread_comment WHERE id=?", $id);
+                $id = $res->fetchColumn();
+                break;
+            case 'z':
+                $table = 'task';
+                $id_key = 'tid';
+                break;
+            case 'zk':
+                $table = 'task';
+                $id_key = 'tid';
+                $anchor = '#zk' . $id;
+
+                /** @var helper_plugin_sqlite $sqlite */
+                $sqlite = plugin_load('helper', 'bez_db')->getDB();
+                $res = $sqlite->query("SELECT task_id FROM task_comment WHERE id=?", $id);
+                $id = $res->fetchColumn();
+                break;
+        }
+
+        return array($match, $table, $id_key, $id, $anchor);
     }
 
-    function render($mode, Doku_Renderer $renderer, $link) {
+    function render($mode, Doku_Renderer $renderer, $data) {
 		if ($mode == 'xhtml') {
 			$id = $_GET['id'];
 			$ex = explode(':', $id);
@@ -39,12 +66,10 @@ class syntax_plugin_bez_qlink extends DokuWiki_Syntax_Plugin {
 				case 'en':
 					$lang_code = $ex[0].':';
 			}
-			$nr = $link[1];
-			if ($link[0] === 'z') {
-				$renderer->doc .= '<a href="?id='.$lang_code.'bez:task:tid:'.$nr.'">#z'.$nr.'</a>';
-			} else {
-				$renderer->doc .= '<a href="?id='.$lang_code.'bez:thread:id:'.$nr.'">#'.$nr.'</a>';
-			}
+
+            list($match, $table, $id_key, $id, $anchor) = $data;
+            $renderer->doc .= '<a href="?id='.$lang_code.'bez:'.$table.':'.$id_key.':'.$id.$anchor.'">'.$match.'</a>';
+
 			return true;
 		}
 		return false;
