1a0fddfed97b69d277709f4ad6524dfc080551a9
Szymon Olewniczak <solewniczak@rid.pl>
2019-07-23 09:38:33 +0200

    error handling for cron functions

--- cron-daily.php
+++ cron-daily.php
@@ -8,16 +8,28 @@ define('DOKU_INC', $inc.'/');
 require_once(DOKU_INC.'inc/init.php');
 
 if (empty($conf['baseurl'])) {
-    echo "set baseurl in dokuwiki config\n";
+    echo "set baseurl in dokuwiki config: ${conf['basedir']}\n";
     exit(-1);
 }
 
 require_once 'cron/functions.php';
 
-if (date('l') === $conf['plugin']['bez']['weekly_cron_day_of_the_week']) {
-    send_weekly_message();
+$errors = [];
+try {
+    if (date('l') === $conf['plugin']['bez']['weekly_cron_day_of_the_week']) {
+        send_weekly_message();
+    }
+
+    send_task_reminder();
+    send_inactive_issue();
+} catch (Exception $e) {
+    $errors[] = $e->getMessage();
 }
 
-send_task_reminder();
+if ($errors) {
+    echo $conf['baseurl'] . ":\n";
+    echo implode("\n", $errors);
+    echo "\n";
+    exit(-1);
+}
 
-send_inactive_issue();
